58a3ea14e72cf8c9c713bb210a3f1c14eb8b0732
Il Sik Yoon <yis9595@gmail.com>
2025-10-07 14:03:59 +0900

    first commit

--- VSPractice/VSPractice/Program.cs
+++ VSPractice/VSPractice/Program.cs
@@ -15,7 +15,12 @@ Task t1 = Task.Run(rankingTest[0].DoRankingTest);
 Task t2 = Task.Run(rankingTest[1].DoRankingTest);
 Task t3 = Task.Run(rankingTest[2].DoRankingTest);
 
-Console.WriteLine("Task 모두 호출 완료");
+while (Console.ReadLine() != "Q")
+{
+    Thread.Sleep(1000);
+}
+//키 입력 받고 성공하면
+RankingManager.ExitRankingTestThread(); //쓰레드 종료 시키기
 
 Task.WhenAll(t1, t2, t3).Wait();
 
--- VSPractice/VSPractice/RankingManager.cs
+++ VSPractice/VSPractice/RankingManager.cs
@@ -6,6 +6,10 @@ public class RankingManager
 {
     public SessionManager sessionManager;
     public NetworkManager networkManager;
+
+    //todo//100위 전체 랭킹이 들어오는 자료구조가 하나 있어야 함 
+
+    private static EventWaitHandle _stopEvent = new ManualResetEvent(false);
     public RankingManager()
     {
         sessionManager = new SessionManager(1000);
@@ -17,6 +21,12 @@ public class RankingManager
         networkManager.ConnectRankingServer();
         return true;
     }
+    bool GetRanking()
+    {
+        //내 서버 랭킹 자료구조에 넣어줌 
+
+        return true;
+    }
 
     bool UpdateRanking(UInt64 ID, uint level)
     {
@@ -26,7 +36,11 @@ public class RankingManager
 
         return true;
     }
-
+    public static void ExitRankingTestThread()
+    {
+        Console.WriteLine("RakingThread 종료 시그널");
+        _stopEvent.Set();   
+    }
 
     public void DoRankingTest() //쓰레드 함수
     {
@@ -34,33 +48,27 @@ public class RankingManager
 
         ConenctRedis();
 
-        int count = 0;
-
         UInt64 resultID = 0;
         uint resultLevel = 0;
         bool bLevelUp = false;
 
         while (true)
         {
-            count++;
-            //종료에 대한 건 이벤트 시그널 확인
             bLevelUp = sessionManager.RandomPlayerLevelUp(out resultID, out resultLevel);
             if (bLevelUp == true)
             {
                 UpdateRanking(resultID, resultLevel);
             }
-            
-            //Frame Sleep
 
-
-            if (count == 20)
+            if (_stopEvent.WaitOne(0)) //종료 시그널 확인
             {
                 break;
             }
-            Thread.Sleep(10000);
+
+            Thread.Sleep(100);
         }
 
-
+        Console.WriteLine($"Ranking thread 종료 : {Thread.CurrentThread.ManagedThreadId}");
     }
 
 }
